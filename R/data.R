09f1e6f15d793a6eb5f0436f0829bf1be1b7eadf
Paul <paul.whyte2@gmail.com>
2018-01-01 21:49:43 +0000

    creation of data documentation data.R

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' Information about select air restaurants.
+#'
+#' The columns are as follows:
+#' \item{air_store_id}{Store identifier}
+#' \item{air_genre_name}{Store genre}
+#' \item{air_area_name}{Area of the store}
+#' \item{latitude}
+#' \item{longitude}
+
+"air_store_info"
